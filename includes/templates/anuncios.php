413913dc4267281bcbf709ef09d28de0e28d4e84
NeilChavez <lolochavez22@icloud.com>
2023-07-06 17:56:17 -0500

    refactor: separate the notice-board into another file

---
+++ includes/templates/anuncios.php
@@ -0,0 +1,33 @@
+   <div class="contenedor-anuncios">
+          <div class="anuncio">
+            <picture>
+              <source srcset="./build/img/anuncio1.webp" type="image/webp">
+              <source srcset="./build/img/anuncio1.jpg" type="image/jpg">
+              <img loading="lazy" src="./build/img/anuncio1.jpg" alt="anuncio">
+            </picture>
+            <div class="contenido-anuncio">
+              <h3>Casa de lujo en el lago </h3>
+              <p>Casa en el lago con excelente vision, acabados de lujo a un excelente precio</p>
+
+              <p class="precio">$3.000.000</p>
+
+              <ul class="iconos-caracteriticas">
+                <li>
+                  <img class="icono" loading="lazy" src="./build/img/icono_wc.svg" alt="icono wg">
+                  <p>3</p>
+                </li>
+                <li>
+                  <img class="icono" loading="lazy" src="./build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                  <p>3</p>
+                </li>
+                <li>
+                  <img class="icono" loading="lazy" src="./build/img/icono_dormitorio.svg" alt="icono dormitorio">
+                  <p>3</p>
+                </li>
+              </ul>
+              <a href="anuncio.php" class="boton boton-amarillo-block">
+                Ver Propriedad
+              </a>
+            </div> <!-- .contenido-anuncio-->
+          </div><!--anuncio-->
+        </div> <!--.contenedor-anuncios-->
